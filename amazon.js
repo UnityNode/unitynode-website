9440add4e195763d3794a2871b623b1f2c7514b5
UnityNode <michealakinbolawa@gmail.com>
2025-12-02 09:51:37 -0800

    Add files via upload

---
+++ amazon.js
@@ -0,0 +1,97 @@
+import {cart, addToCart} from "../data/cart.js";
+import {products} from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
+
+
+let productHTML ='';
+
+products.forEach( (products) => {
+
+ productHTML += `
+ <div class="card">
+          <div class="product-container">
+                <div class="product-image-container">
+                  <img class="product-image"
+                    src="${products.image}">
+                </div>
+
+                <div class="product-name limit-text-to-2-lines">
+                 ${products.name}
+                </div>
+
+                <div class="product-rating-container">
+                  <img class="product-rating-stars"
+                    src="images/ratings/rating-${(products.rating.stars)*10}.png">
+                  <div class="product-rating-count link-primary">
+                    ${products.rating.count}
+                  </div>
+                </div>
+
+                <div class="product-price">
+                 $${formatCurrency(products.priceCents)}
+                </div>
+
+                <div class="product-quantity-container">
+                  <select class="js-quantity-selector-${products.id}">
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                  </select>
+                </div>
+
+                <div class="product-spacer"></div>
+
+                <div class="added-to-cart">
+                  <img src="images/icons/checkmark.png">
+                  Added
+                </div>
+
+                <button class="add-to-cart-button button-primary js-add-to-cart"
+                data-product-id="${products.id}">
+                  Add to Cart
+                </button>
+          </div>
+</div>          `
+          document.querySelector('.js-products-grid').innerHTML = `${productHTML}`
+})
+ 
+
+function updateQuantity() {
+      let cartQuantity = 0;
+
+      cart.forEach((cartItem) => {
+      cartQuantity  += cartItem.quantity 
+          });
+console.log(cartQuantity)
+ document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+ }
+ 
+
+ document.querySelectorAll('.js-add-to-cart')
+  .forEach( (button) => {
+    button.addEventListener('click', () => {
+      let productId = button.dataset.productId;
+
+      addToCart(productId);
+
+      updateQuantity();
+
+       console.log(cart)
+
+  /*
+       let dropDownQuantity = document.querySelector(`.js-quantity-selector-${products.id}`)
+       let num =  dropDownQuantity.value 
+ 
+       console.log(num)
+  */
+ 
+    })
+  }
+)
